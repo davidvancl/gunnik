877b74017a794da98a710850e939a80213b462c7
Daniel Kňourek <knourek.daniel@gmail.com>
2023-04-25 19:22:18 +0000

    fix: gunnik unused variable

--- gunnik/server/server.ts
+++ gunnik/server/server.ts
@@ -10,10 +10,6 @@ gun.on('hi', peer => {
     console.log(`Nový klient se připojil s ID ${peer.id} a adresou ${peer.url}`)
 })
 
-let opts: GunOptions = {
-
-}
-
 server.on('connect', () => {
     console.log("client connected");
 
