419dc6fb5e918024ca4630f624f6ec78c159c3c0
David Vancl <develop.programme@gmail.com>
2023-04-26 19:03:43 +0000

    Rework client to webpack based app with - React,TypeScript,Webpack,Babel,ESLint,Prettier

--- gunnik/client/src/App.tsx
+++ gunnik/client/src/App.tsx
@@ -1,41 +1,8 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import GUN, { IGunInstance } from "gun";
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import GunnikComponent from '@components/GunnikComponent';
+import React from 'react';
 
-function App() {
-  const [text, setText] = useState<string>();
-  const [gun, setGun] = useState<IGunInstance<any>>();
-
-  useEffect(() => {
-    console.log("Starting server ...");
-    setGun(GUN("http://147.230.236.150:8765/gun"));
-  }, []);
-
-  useEffect(() => {
-    if (gun !== undefined) {
-      console.log("Registering event ...");
-      gun.get('mark').on((data, key) => {
-        setText(data.live);
-      });
-    }
-  }, [gun]);
-
-  const updateText = (event: any) => {
-    setText(event.target.value);
-    if (gun !== undefined) {
-      gun.get('mark').get('live').put(event.target.value);
-    }
-  }
-
-  return <div className="App">
-    <InputGroup>
-      <InputGroup.Text>With textarea</InputGroup.Text>
-      <Form.Control as="textarea" value={text} onChange={updateText} aria-label="With textarea" />
-    </InputGroup>
-  </div>;
-}
-
-export default App;
+export const App: React.FC = () => (
+  <>
+    <GunnikComponent />
+  </>
+);
