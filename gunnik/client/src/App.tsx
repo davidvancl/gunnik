0fe4446ea8191f158793a8000e4e8d59da060a2e
David Vancl <develop.programme@gmail.com>
2023-05-06 19:08:42 +0000

    add scss, setting context, base UI, webpack config

--- gunnik/client/src/App.tsx
+++ gunnik/client/src/App.tsx
@@ -1,8 +1,30 @@
-import GunnikComponent from '@components/GunnikComponent';
-import React from 'react';
+import GunSettingsComponent from '@components/GunSettingsComponent';
+import { ISettings, SettingsProvider } from '@contexts/SettingsContext';
+import React, { useEffect, useState } from 'react';
 
-export const App: React.FC = () => (
-  <>
-    <GunnikComponent />
-  </>
-);
+export const App: React.FC = () => {
+  const [settings, setSettings] = useState<ISettings | null>(null);
+  const settingsHook = { settings, setSettings };
+
+  useEffect(() => {
+    let localDataSettings = localStorage.getItem('gunnik-settings');
+    if (localDataSettings) {
+      let localSettings: ISettings = JSON.parse(localDataSettings);
+      if (localSettings) {
+        setSettings(localSettings);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('gunnik-settings', JSON.stringify(settings));
+  }, [settings]);
+
+  return (
+    <div className="main-border">
+      <SettingsProvider value={settingsHook}>
+        <GunSettingsComponent />
+      </SettingsProvider>
+    </div>
+  )
+}
